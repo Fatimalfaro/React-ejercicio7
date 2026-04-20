6239e4115672f4e13c0aa3d0f9c1bee967d3fec4
Fátima Alfaro <fatialfaro99@gmail.com>
2026-04-20 18:48:29 -0300

    Limpie la estructura del proyecto y agregue los componentes navbar y footer

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar bg-primary">
+      <div className="container">
+        <span className="navbar-brand mb-0 h1 text-center">Práctica de React</span>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
